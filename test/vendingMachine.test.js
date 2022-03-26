3973eac2768ef609406b6cd79f642b1f59405e67
Ishola Azeez <isholaazeezayo@gmail.com>
2022-03-26 09:36:10 +0100

    My blockchain project

--- test/vendingMachine.test.js
+++ test/vendingMachine.test.js
@@ -8,4 +8,13 @@ contract("VendingMachine", (accounts) => {
         let balance = await instance.getVendingMachineBalance()
         assert.equal(balance, 100, "the initial balance should be 100")
     });
+    it('ensures the balance of the vending machine can be updated', async () => {
+        await instance.restock(100)
+        let balance = await instance.getVendingMachineBalance()
+        assert.equal(balance, 200, "the balance should be 200 after restocking")
+    });
+    it('allow donut to be purchase', async () => {
+        await instance.purchase(1, {from:accounts[0], value: web3.utils.toWei('3', 'ether')});
+        assert.equal(balance, 190, 'the balance should be 190')
+    })
 })
